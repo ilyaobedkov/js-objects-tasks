4c848dd213166aec00b2f737dd108abddb9e5c82
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 22:00:20 +0300

    6-for-of.js

--- solutions/6-for-of.js
+++ solutions/6-for-of.js
@@ -1,3 +1,10 @@
-// BEGIN
-
-// END
+export default function pick(obj, keys) {
+  const result = {};
+  for (const key of keys) {
+    // Проверяем, существует ли свойство в исходном объекте
+    if (key in obj) {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+}
