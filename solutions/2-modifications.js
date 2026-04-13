dd6a65c45dc84f4429d074ab102505b75cc21f67
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 21:58:24 +0300

    2-modifications.js

--- solutions/2-modifications.js
+++ solutions/2-modifications.js
@@ -1,5 +1,11 @@
 import _ from 'lodash';
-
-// BEGIN
-
-  // END
+export default function normalize(lesson) {
+  // Капитализируем имя с помощью lodash
+  if (lesson.name) {
+    lesson.name = _.capitalize(lesson.name);
+  }
+  // Приводим описание к нижнему регистру
+  if (lesson.description) {
+    lesson.description = lesson.description.toLowerCase();
+  }
+}
