6a56d47e4f4ea1483a5d3916a20e4003d1ccd314
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 21:58:52 +0300

    3-references.js

--- solutions/3-references.js
+++ solutions/3-references.js
@@ -1,3 +1,6 @@
-// BEGIN
-
-// END
+export default function is(company1, company2) {
+  // Сравниваем три ключевых поля
+  return company1.name === company2.name &&
+         company1.state === company2.state &&
+         company1.website === company2.website;
+}
