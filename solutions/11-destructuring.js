de5c38d800d27103d8e8e1c900c5269033200b84
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 22:02:05 +0300

    11-destructuring.js

--- solutions/11-destructuring.js
+++ solutions/11-destructuring.js
@@ -1,3 +1,9 @@
-// BEGIN
-
-// END
+export default function getSortedNames(users) {
+  // Извлекаем имена с помощью деструктуризации
+  const names = [];
+  for (const { name } of users) {
+    names.push(name);
+  }
+  // Сортируем имена в алфавитном порядке
+  return names.sort();
+}
