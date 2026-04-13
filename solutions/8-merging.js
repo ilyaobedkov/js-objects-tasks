51ec66b1b4efc75dd43946ab8a931503070e412b
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 22:01:01 +0300

    8-merging.js

--- solutions/8-merging.js
+++ solutions/8-merging.js
@@ -1,5 +1,11 @@
 import _ from 'lodash';
-
-// BEGIN
-
-// END
+export default function fill(target, keys, source) {
+  // Если список ключей пустой, сливаем все данные
+  if (keys.length === 0) {
+    Object.assign(target, source);
+  } else {
+    // Иначе выбираем только указанные ключи из source
+    const dataToMerge = _.pick(source, keys);
+    Object.assign(target, dataToMerge);
+  }
+}
