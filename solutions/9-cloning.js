333c28aaed50e388923805e96a39f53e0e36f89e
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 22:01:23 +0300

    9-cloning.js

--- solutions/9-cloning.js
+++ solutions/9-cloning.js
@@ -1,3 +1,10 @@
-// BEGIN
-
-// END
+export default function cloneShallow(obj) {
+  const clone = {};
+  for (const key in obj) {
+    // Проверяем, что свойство принадлежит самому объекту, а не прототипу
+    if (Object.hasOwn(obj, key)) {
+      clone[key] = obj[key];
+    }
+  }
+  return clone;
+}
