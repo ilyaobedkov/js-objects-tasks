a3653a8c6708f1a6eb8006ce8a4410ac374cd3c6
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 21:59:24 +0300

    4-using.js

--- solutions/4-using.js
+++ solutions/4-using.js
@@ -1,3 +1,17 @@
-// BEGIN
-
-// END
+export default function getDomainInfo(domain) {
+  // Определяем протокол по умолчанию
+  let scheme = 'http';
+  let name = domain;
+  // Проверяем, начинается ли строка с 'https://'
+  if (domain.startsWith('https://')) {
+    scheme = 'https';
+    name = domain.replace('https://', '');
+  } 
+  // Проверяем, начинается ли строка с 'http://'
+  else if (domain.startsWith('http://')) {
+    scheme = 'http';
+    name = domain.replace('http://', '');
+  }
+  // Возвращаем объект с результатами
+  return { scheme, name };
+}
