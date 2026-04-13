bd30f4784c5e63a27b2901905cf665f61d82c9f8
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 21:57:57 +0300

    1-syntax.js

--- solutions/1-syntax.js
+++ solutions/1-syntax.js
@@ -1,3 +1,7 @@
-// BEGIN
-
-// END
+import fs from 'fs';
+import path from 'path';
+export default function getJsonFileData() {
+  const filePath = path.join(process.cwd(), 'tasks', 'example.json');
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(fileContent);
+}
