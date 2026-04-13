a4eb0b1c491cee991c8c085206b12681e3d8b2b1
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-04-13 21:59:40 +0300

    5-property-existence.js

--- solutions/5-property-existence.js
+++ solutions/5-property-existence.js
@@ -1,5 +1,20 @@
 import _ from 'lodash';
-
-// BEGIN
-
-// END
+export default function countWords(sentence) {
+  // Разбиваем предложение на слова с помощью _.words
+  const words = _.words(sentence);
+  // Создаем объект для хранения результатов
+  const result = {};
+  // Проходим по каждому слову
+  for (const word of words) {
+    // Приводим слово к нижнему регистру
+    const lowerWord = word.toLowerCase();
+    // Если слово уже есть в объекте, увеличиваем счетчик
+    if (result[lowerWord]) {
+      result[lowerWord] += 1;
+    } else {
+      // Если слова нет, добавляем его со значением 1
+      result[lowerWord] = 1;
+    }
+  }
+  return result;
+}
